ea0215f0731b1da6e6c1384da6d5fc746f207f6a
GreeM09 <119611816+GreeM09@users.noreply.github.com>
2025-03-15 00:15:26 +0500

    Update 2.py

--- lab_4/data_python/2.py
+++ lab_4/data_python/2.py
@@ -4,10 +4,3 @@ time_now = dt.now()
 print(f'yesterday: {time_now.day - 1}th {time_now.strftime("%B")}'
       f'\ntoday: {time_now.day}th {time_now.strftime("%B")}'
       f'\ntommorow: {time_now.day + 1}th {time_now.strftime("%B")}')
-
-
-'''
-Yesterday, all my troubles seemed so far away,
-Now it looks as though they're here to stay,
-Oh, I believe in yesterday
-'''
